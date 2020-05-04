b56b5c834d5d1c8ca3280e550e538101d27dca65
snytav <snytav@gmail.com>
2020-05-04 23:22:50 +0700

    js plus 1 OK

--- GetDensity.py
+++ GetDensity.py
@@ -9,12 +9,21 @@ def GetDensity( r, L, J ):
     js = np.floor(r/dx)
     #return js
     ys = r/dx - (js)
+    #js -= 1
     #return ys
     n1 =  ys
-    js_plus_1 = np.mod(js,J)+1
+
+    j = np.mod(js,J)+1
+    j = j + J*(j<0) - J*(j>J)
+    js_plus_1 = j
+
+
     #return js_plus_1
     n1 = accum(js.astype(int),(1-ys)/dx)
     np.savetxt('n1.txt', n1, delimiter='\n',fmt='%15.5e')
+    np.savetxt('js.txt', js+1,delimiter='\n',fmt='%15d')
+    np.savetxt('js_plus_1.txt', js_plus_1+1, delimiter='\n',fmt='%15d')
+    np.savetxt('ys_dx.txt', ys/dx, delimiter='\n',fmt='%15.5e')
     n2 = accum(js_plus_1.astype(int),ys/dx)
     np.savetxt('n2.txt', n2, delimiter='\n',fmt='%15.5e')
     n = n1 + n2
